ca5d7473b754b57c08baf084bd325b4402da1573
denislav95 <zotevdenislav@gmail.com>
2016-08-18 17:29:25 +0300

    initialize

---
+++ ASPNETWebApplicationProject/ASPNETWebApplicationProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASPNETWebApplicationProject.Startup))]
+namespace ASPNETWebApplicationProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
